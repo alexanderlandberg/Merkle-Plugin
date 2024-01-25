e87bf1db3280dcfc494b1b558361abb3a388b85b
alexanderlandberg <gollum.dreng@hotmail.com>
2024-01-25 16:26:49 +0100

    Added function for replacing images

--- whoiswho/script.js
+++ whoiswho/script.js
@@ -2,11 +2,11 @@ setInterval(() => {
     let dullanSelector = document.querySelector(".dullan");
     if (!dullanSelector) {
         addDullan();
+        addNewImages();
     }
 }, 10);
 
 function addDullan() {
-    console.log("add dullan");
     let html = `<div class="o-userPreview o-usersList__user" wire:click="$emitTo('organisms.user-details', 'show', '33d3e70f-60e1-4b21-8126-d543ae4e8777')">
         <div class="o-userPreview__imageContainer">
             <div class="o-userPreview__image"></div>
@@ -107,4 +107,40 @@ function addMoreDullan() {
     setTimeout(() => {
         addMoreDullan();
     }, 1000);
+}
+
+function addNewImages() {
+    const profileList = [
+        {
+            "name": "Bobby Hollingsworth",
+            "img": "https://alexanderlandberg.github.io/merkle-gifs/gifs/bobby.gif"
+        },
+        {
+            "name": "Thomas Gjermansen",
+            "img": "https://alexanderlandberg.github.io/merkle-gifs/gifs/tbone-scratch.gif"
+        },
+        {
+            "name": "Sune Brodersen",
+            "img": "https://hips.hearstapps.com/hmg-prod/images/elvis-presley-american-rock-n-roll-legend-news-photo-1690830848.jpg"
+        },
+        {
+            "name": "Ulrik Mailand",
+            "img": "https://texasarchive.org/sites/default/files/images_end/test/2015_01674_tn.jpg"
+        },
+        {
+            "name": "Nicklas Frandsen",
+            "img": "https://img6.fresherslive.com/latestnews/2023/05/taylor-lautner-ethnicity-what-is-taylor-lautner-ethnicity-6476c35b8b71e63598516-900.webp"
+        }
+    ]
+    const profileNameList = document.querySelectorAll(".a-lead.o-userPreview__name");
+    for (let i = 0; i < profileNameList.length; i++) {
+        for (let j = 0; j < profileList.length; j++) {
+            console.log("TEST")
+            if (profileNameList[i].innerHTML.includes(profileList[j].name)) {
+                let profile = profileNameList[i].closest(".u-col-12");
+                profile.querySelector(".o-userPreview__image").style.backgroundImage = `url(${profileList[j].img})`;
+                profile.querySelector(".o-userPreview__image").style.filter = "grayscale(1)";
+            }
+        }
+    }
 }
